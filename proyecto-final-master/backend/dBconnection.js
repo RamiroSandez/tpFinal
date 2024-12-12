ce70b57948c3fc50cc12de1005b52771a3c40658
RamiroSandez <129991198+RamiroSandez@users.noreply.github.com>
2024-12-12 15:30:27 -0300

    Update dBconnection.js

--- proyecto-final-master/backend/dBconnection.js
+++ proyecto-final-master/backend/dBconnection.js
@@ -1,15 +1,9 @@
 require("dotenv").config({ path: "../.env" });
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: process.env.DB_DIALECT || "mysql",
-  }
-);
+const connection = new Sequelize(process.env.MYSQL_URL, {
+  dialect: process.env.DB_DIALECT || "mysql",
+});
 
 connection
   .authenticate()
